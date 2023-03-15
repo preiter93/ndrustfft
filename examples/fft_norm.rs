9840bde5326a243c0e3da461917476914d631dc2
Philipp Reiter <philipp.reiter@freiheit.com>
2023-03-15 08:10:02 +0100

    add more examples for custom normalization

--- examples/fft_norm.rs
+++ examples/fft_norm.rs
@@ -1,5 +1,5 @@
-//! Perform 1-dimensional Fourier Transform with custom
-//! normalization.
+//! Perform 1-dimensional Fourier Transform with
+//! different normalizations
 //!
 //! cargo run --example fft_norm
 use ndarray::{array, Array1};
@@ -7,25 +7,34 @@ use ndrustfft::{ndfft, ndifft, Complex, FftHandler, Normalization};
 
 fn main() {
     let n = 3;
-
-    // Init arrays
     let v: Array1<Complex<f64>> = array![1., 2., 3.].mapv(|x| Complex::new(x, x));
     let mut vhat: Array1<Complex<f64>> = Array1::zeros(n);
     let mut v2: Array1<Complex<f64>> = Array1::zeros(n);
 
-    // define custom normalization
-    fn custom_norm(data: &mut [Complex<f64>]) {
-        let n = 2. / data.len() as f64;
-        for d in data.iter_mut() {
-            *d = *d * n;
-        }
-    }
-    let norm = Normalization::Custom(custom_norm);
+    println!("{:?}", v); // 1+1.j, 2+2.j, 3+3.j
 
-    // Init handler
-    let mut handler = FftHandler::<f64>::new(n).normalization(norm);
-
-    // Perform transforms
-    ndfft(&v, &mut vhat, &mut handler, 0);
+    // Default normalization
+    let mut handler = FftHandler::<f64>::new(n).normalization(Normalization::Default);
+    ndfft(&v.clone(), &mut vhat, &mut handler, 0);
     ndifft(&vhat, &mut v2, &mut handler, 0);
+    println!("{:?}", v2); // // 1+1.j, 2+2.j, 3+3.j
+
+    // No normalization
+    let mut handler = FftHandler::<f64>::new(n).normalization(Normalization::None);
+    ndfft(&v.clone(), &mut vhat, &mut handler, 0);
+    ndifft(&vhat, &mut v2, &mut handler, 0);
+    println!("{:?}", v2); // 3+3.j, 6+6.j, 9+9.j
+
+    // Custom normalization
+    let mut handler = FftHandler::<f64>::new(n).normalization(Normalization::Custom(my_norm));
+    ndfft(&v.clone(), &mut vhat, &mut handler, 0);
+    ndifft(&vhat, &mut v2, &mut handler, 0);
+    println!("{:?}", v2); // // 2+2.j, 4+4.j, 6+6.j
+}
+
+fn my_norm(data: &mut [Complex<f64>]) {
+    let n = 2. / data.len() as f64;
+    for d in data.iter_mut() {
+        *d = *d * n;
+    }
 }
